8efb6934ba6e6367c82df5110bf360e8459f59e1
roshan <roshanahegde@gmail.com>
2025-01-24 16:07:56 -0600

    removed magic version number and using pname over name

--- package.nix
+++ package.nix
@@ -7,17 +7,18 @@
 , libX11
 , libXt
 }: let
+  # JNativeHook libraries needed for global hotkeys
   JNHookLibs = [
     libxkbcommon
     libX11
     libXt
   ];
 in stdenv.mkDerivation rec {
-  name = "NinjaBrain-Bot";
+  pname = "NinjaBrain-Bot";
   version = "1.5.1";
 
   src = fetchurl {
-    url = "https://github.com/Ninjabrain1/${name}/releases/download/1.5.1/${name}-${version}.jar";
+    url = "https://github.com/Ninjabrain1/${pname}/releases/download/${version}/${pname}-${version}.jar";
     hash = "sha256-Rxu9A2EiTr69fLBUImRv+RLC2LmosawIDyDPIaRcrdw=";
   };
 
@@ -28,10 +29,10 @@ in stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -pv $out/share/java $out/bin
-    cp ${src} $out/share/java/${name}-${version}.jar
+    cp ${src} $out/share/java/${pname}-${version}.jar
 
-    makeWrapper ${jdk}/bin/java $out/bin/${name} \
-    --add-flags "-jar $out/share/java/${name}-${version}.jar" \
+    makeWrapper ${jdk}/bin/java $out/bin/${pname} \
+    --add-flags "-jar $out/share/java/${pname}-${version}.jar" \
     --set LD_LIBRARY_PATH ${lib.makeLibraryPath JNHookLibs }
   '';
 
